e9b87e2315cd02735d19ffd482a1c00308e05598
Saman <kaursaman242@email.com>
2025-11-25 11:33:07 +0530

    Add digital clock and to-do list functionality with corresponding HTML, CSS, and JS files

---
+++ digital-clock.js
@@ -0,0 +1,34 @@
+// // const time=document.getElementsByClassName("clock")
+// const utcTime= new Date() // utc
+// const localTime= new Date(utcTime)
+
+// setInterval(() => {
+//     console.log(new Date().toLocaleTimeString());
+// }, 1000);
+ 
+// const time=document.getElementsByClassName("clock")
+// time.append()
+
+
+        function updateClock() {
+            let now = new Date();
+
+            let hours = now.getHours();
+            let minutes = now.getMinutes();
+            let seconds = now.getSeconds();
+
+          
+            hours = hours < 10 ? "0" + hours : hours;
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            let time = `${hours}:${minutes}:${seconds}`;
+
+            document.getElementById("clock").textContent = time;
+        } 
+
+        updateClock();
+        setInterval(updateClock, 1000);
+
+        
+   
